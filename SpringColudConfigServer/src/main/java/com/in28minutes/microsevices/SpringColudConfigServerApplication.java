1713242cec690e0c66e2c51d195b67793560b324
AbhideepRND <samtel09@gmail.com>
2019-02-24 09:42:23 +0530

    Application Config and Spring config server

---
+++ SpringColudConfigServer/src/main/java/com/in28minutes/microsevices/SpringColudConfigServerApplication.java
@@ -0,0 +1,13 @@
+package com.in28minutes.microsevices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringColudConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringColudConfigServerApplication.class, args);
+	}
+
+}
