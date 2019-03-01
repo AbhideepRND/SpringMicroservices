a9d46cfead354e9b80473b30e689bdad0333cdcb
AbhideepRND <samtel09@gmail.com>
2019-03-01 19:59:49 +0530

    Configuration for eureka service

--- CurrencyConversionService/src/main/java/com/in28minutes/microsevices/CurrencyConversionServiceApplication.java
+++ CurrencyConversionService/src/main/java/com/in28minutes/microsevices/CurrencyConversionServiceApplication.java
@@ -2,10 +2,12 @@ package com.in28minutes.microsevices;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients(basePackages= {"com.in28minutes.microsevices.proxy"})
+@EnableDiscoveryClient
 public class CurrencyConversionServiceApplication {
 
 	public static void main(String[] args) {
